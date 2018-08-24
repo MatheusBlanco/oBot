9d0193330d179551d026fde75a7f699f1f02b522
MatheusBlanco <msallesblanco@gmail.com>
2018-08-24 11:56:21 -0300

    Co-authored-by: gabrielfilipe7unb <gabrielfilipe7unb@gmail.com> Co-authored-by: braga9 <braga.gabriel1000@gmail.com> Co-authored-by: MatheusBlanco <msallesblanco@gmail.com>

--- bot.py
+++ bot.py
@@ -6,6 +6,15 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start','help'])
 def send_welcome(message):
-    bot.reply_to(message, u"Fala zé butão")
+    bot.reply_to(message, u"Oi ameguinho")
+
+@bot.message_handler(func=lambda  message: True)
+def shout_message(message):
+    content = message.text
+    if content == "oi":
+        bot.send_message(chat_id=message.chat.id, text="Eu não faço nada ainda, então joga isso aqui: ")
+        bot.open
+    else:
+        bot.send_message(chat_id=message.chat.id, text="Digita /start de novo")
 
 bot.polling()
